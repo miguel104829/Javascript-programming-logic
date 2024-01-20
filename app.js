46c84202aae6994b555d0633f7cbaa24d9765305
Miguel Samaca <miguel_samaca@hotmail.com>
2024-01-20 13:38:12 -0500

    Add loop, break and ternary operator

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 let numeroSecreto = 7;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraIntentos = "intento";
+let maxIntentos = 3;
 
-numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
-console.log(numeroUsuario);
-if (numeroUsuario == numeroSecreto) {
-  alert("Has acertado!");
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    console.log(numeroUsuario);
+    if (numeroUsuario == numeroSecreto) {
+    alert(`Acertaste, el número es ${numeroUsuario}. lo hiciste en ${intentos} ${palabraIntentos}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+        alert("El número es menor");
+        } else {
+        alert("El número es mayor");
+        }
+        intentos++;
+     palabraIntentos = "intentos";
+        if (intentos > maxIntentos) {
+        alert(`Llegaste al númmero máximo de intentos permitidos de ${maxIntentos}`);
+        break;
+        }
+
+    }
 }
-alert("El número ingresado no es correcto.");
