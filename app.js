12f3cbc705b8755ffe4fc8b3a29037c49227c1a5
Miguel Samaca <miguel_samaca@hotmail.com>
2024-01-20 13:13:07 -0500

    Add variables and conditional

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let numeroSecreto = 7;
+let numeroUsuario = 0;
+
 numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 console.log(numeroUsuario);
-alert("El número que has indicado es: " + numeroUsuario);
+if (numeroUsuario == numeroSecreto) {
+  alert("Has acertado!");
+}
+alert("El número ingresado no es correcto.");
